301347b45fc2925e34056ea8738ed09ae5825c23
Artyom <artyom@ymail.com>
2021-05-01 23:50:37 +0400

    initial commit.

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Editor from './Editor'
+
+function App() {
+  const [html, setHtml] = useState('')
+  const [css, setCss] = useState('')
+  const [js, setJs] = useState('')
+    return (
+      <React.Fragment>
+      <div className="pane top-pane">
+        <Editor
+        language="xml"
+        displayName="HTML"
+        value={html}
+        onChange={setHtml}
+        />
+        <Editor
+        language="css"
+        displayName="CSS"
+        value={css}
+        onChange={setCss}
+        />
+        <Editor
+        language="js"
+        displayName="JS"
+        value={js}
+        onChange={setJs}
+        />
+      </div>
+      <div className="pane">
+        <iframe
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+          >
+        </iframe>
+      </div>
+
+      </React.Fragment>
+    )
+}
+
+export default App;
